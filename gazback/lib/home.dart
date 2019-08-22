92599ca811b8070dfd7bd1755f15ba2ddd09cdbe
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-08-22 20:09:11 +0700

    Icon Launcher

--- gazback/lib/home.dart
+++ gazback/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,6 +7,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final CameraPosition _initialPos = CameraPosition(
+    target: LatLng(-7.7753915, 110.3777944),
+    zoom: 16,
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +20,12 @@ class _HomeScreenState extends State<HomeScreen> {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
+      ),
+      body: GoogleMap(
+        initialCameraPosition: _initialPos,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        
       ),
     );
   }
