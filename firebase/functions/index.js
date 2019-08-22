b1c3607c0bba6fef1967caa7667f8558d5492b55
Tensky <serusial1122@gmail.com>
2019-08-23 00:21:34 +0700

    Cloud Functions n stuff

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,29 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const db = admin.firestore();
+
+exports.syncKendaraan = functions.firestore
+    .document('pengguna/{user_id}/kendaraan/{id}')
+    .onCreate(async (snap, context) => {
+        const data = snap.data();
+        await db.collection('kendaraan').doc(context.params.id).set({
+            plat: data.plat,
+            pengguna_id: context.params.user_id,
+        });
+    });
+
+exports.deleteUser = functions.firestore
+    .document('pengguna/{user_id}/kendaraan/{id}')
+    .onDelete(async (snap, context) => {
+        await db.collection('kendaraan').doc(context.params.id).delete();
+    });
+
+exports.syncTransaksi = functions.firestore
+    .document('pengguna/{user_id}/kendaraan/{id}/pengisian/{transaksi_id}')
+    .onCreate(async (snap, context) => {
+        const data = snap.data();
+        await db.collection('pengguna').doc(context.params.user_id)
+            .collection('transaksi').doc(context.params.transaksi_id).set(data);
+    });
