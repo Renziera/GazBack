8af3a8810e2842d70d5f6f5b86a082c98fd645cc
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-08-23 18:21:11 +0700

    Sebelum berangkat :)

--- gazback/lib/home.dart
+++ gazback/lib/home.dart
@@ -14,6 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
     SizedBox.shrink(),
     SizedBox.shrink(),
   ];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- gazback/lib/page_home.dart
+++ gazback/lib/page_home.dart
@@ -17,6 +17,88 @@ class HomePage extends StatelessWidget {
                       borderRadius: BorderRadius.circular(8))),
             ),
           ),
+          Saldo(),
+        ],
+      ),
+    );
+  }
+}
+
+class Saldo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          colors: [
+            Colors.cyan,
+            Colors.blue,
+          ],
+        ),
+      ),
+      height: 128,
+      padding: EdgeInsets.all(8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('GazPoint', style: TextStyle(color: Colors.white)),
+              Text(
+                '   90.990',
+                style: TextStyle(
+                  color: Colors.yellow,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: <Widget>[
+              Text('Saldo LinkAja',
+                  style: TextStyle(color: Colors.white, fontSize: 24)),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Rp',
+                    style: TextStyle(color: Colors.yellow),
+                  ),
+                  Text(
+                    '3.576.900',
+                    style: TextStyle(
+                      color: Colors.yellow,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Text('Premium Member',
+                  style: TextStyle(color: Colors.white, fontSize: 14)),
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Text('GazCoin', style: TextStyle(color: Colors.white)),
+              Text(
+                '3285   ',
+                style: TextStyle(
+                  color: Colors.yellow,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
